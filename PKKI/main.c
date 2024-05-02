ebbcd37e8a186532e4092a12292de0d1adba1275
Philipp3107 <philippkotte@icloud.com>
2024-05-02 09:51:45 +0200

    Initial Commit

---
+++ PKKI/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PKKI
+//
+//  Created by Philipp Kotte on 02.05.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
